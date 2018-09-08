3e2d77bfc193d330af9af530876322e3785f1439
lane29 <lane29@me.com>
2018-09-08 20:41:39 +1200

    Logic is placed in a separate class - cardTools.java

--- app/src/main/java/com/packtpub/testlayouts/MainActivity.java
+++ app/src/main/java/com/packtpub/testlayouts/MainActivity.java
@@ -1,6 +1,7 @@
 package com.packtpub.testlayouts;
 
 import android.media.MediaPlayer;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,9 @@ import org.w3c.dom.Text;
 
 import java.sql.Time;
 import java.util.Random;
+import java.util.Timer;
+
+import com.packtpub.testlayouts.cardTools;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -38,77 +42,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private MediaPlayer flipMediaPlayer;
 
-    boolean isAnimationInProgress = false;
+    boolean isPlayStarted = false;
+    boolean isOpenedCardsValuesArrayInitiated = true;
+
+    TextView textTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initGame();
+        textTime = (TextView) findViewById(R.id.textTime);
+        textTime.setText("Time: 00:00");
 
-        shuffleCards();
-        turnAllCardsFaceDown();
-    }
-
-
-
-    private void initGame(){
-       // Log.i("info", "initGame...............");
         numOfMatchedCards = 2;
         numRows = 3;
         numCols = 4;
 
-        lengthOfPack = numRows * numCols;           //For level #1 = 12 cards are laid
+        initGame();
+        cardTools.shuffleCards(pack);
 
+/*        for (int i=0; i<pack.length; i++) {
+            Log.i("info", "onCreate...............pack["+i+"]="+pack[i]);
+        }*/
+        turnAllCardsFaceDown();
+    }
+
+    private void initGame(){
+        // Log.i("info", "initGame...............");
+        lengthOfPack = numRows * numCols;           //For level #1 = 12 cards are laid
         numberOfCardsInSet = lengthOfPack / numOfMatchedCards;    //For level #1 = 6 cards in a set
 
-        pack = new int[lengthOfPack];   //Array for all cards
-        initPackArray();
+        //Array for all cards
+        pack = cardTools.initPackArray(lengthOfPack, numberOfCardsInSet);
 
-        playedCards = new boolean[lengthOfPack];   //Array for flags for played cards
-        initPlayedCardsArray();
+        //Array for flags for played cards
+        playedCards = cardTools.initPlayedCardsArray(lengthOfPack);;
 
-        openedCardsValues = new int[numOfMatchedCards];   //Array for opened cards
-        initOpenedCardsValuesArray();
+        //Array for opened cards
+        openedCardsValues = cardTools.initOpenedCardsValuesArray(numOfMatchedCards);
+        isOpenedCardsValuesArrayInitiated = true;
 
-        openedCardsPositions = new int[numOfMatchedCards];   //Array for positions of opened cards
-        initOpenedCardsPositionsArray();
+        //Array for positions of opened cards
+        openedCardsPositions = cardTools.initOpenedCardsPositionsArray(numOfMatchedCards);
 
+        initTextMoves();
         setOnClickListenerOnImageViews();
-
-        initGameTime();
-
-        initMoves();
+        isPlayStarted = false;
     }
 
-    private void initPlayedCardsArray() {
-        // Log.i("info", "initPlayedCardsArray...............");
-        for (int i = 0; i < lengthOfPack; i++) {
-            playedCards[i] = false;
-        }
-    }
-
-    public void initPackArray(){
-       // Log.i("info", "initPackArray...............");
-        for (int i = 0; i < lengthOfPack; i++) {    //Initialising pack array. For level #1 = [1;2;3;4;5;6;1;2;3;4;5;6]
-            pack[i] = (i + 1) % numberOfCardsInSet + 1;
-        }
-    }
-
-
-    public void initOpenedCardsValuesArray(){
-       /// Log.i("info", "initOpenedCardsValuesArray...............");
-        for (int i = 0; i < numOfMatchedCards; i++) {
-            openedCardsValues[i] = 0;
-        }
-    }
-
-    private void initOpenedCardsPositionsArray() {
-        /// Log.i("info", "initOpenedCardsPositionsArray...............");
-        for (int i = 0; i < numOfMatchedCards; i++) {
-            openedCardsPositions[i] = 0;
-        }
+    private void initTextMoves() {
+        cntMoves = 0;
+        TextView textMoves = findViewById(R.id.textMoves);
+        String str = "Moves: 0";
+        textMoves.setText(str);
     }
 
     private void setOnClickListenerOnImageViews() {
@@ -123,98 +110,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
     }
 
-
-    public void shuffleCards() {
-       // Log.i("info", "shuffleCards...............");
-        Random random = new Random();
-
-        for (int i = 0; i < lengthOfPack; i++) {
-            int cardPosition = random.nextInt(lengthOfPack - 1);
-            int curCard = pack[i];
-            pack[i] = pack[cardPosition];
-            pack[cardPosition] = curCard;
-        }
-    }
-
-
-    public void onClick(View view) {
-        Log.i("info", "onClick...............");
-
-
-        //TODO! Timing
-        ///if (timeInMilliseconds == 0) {
-            //start timer
-        //}
-
-        ImageView clickedCard = (ImageView) view;
-
-        //Position of the clicked card in a whole pack
-        int positionInPack = Integer.parseInt(clickedCard.getTag().toString());//10
-
-        Log.i("info", "Position in Pack = " + positionInPack);
-
-        if (!isAnimationInProgress) {
-
-            //Make sound
-            flipMediaPlayer = MediaPlayer.create(this, R.raw.memory_flip);
-            flipMediaPlayer.start();
-
-            Log.i("info", "image" + pack[positionInPack - 1]);
-            //Turn the card face up (name of the target resource looks like "image<X>")
-            int id = getResources().getIdentifier("image" + pack[positionInPack - 1], "drawable", getPackageName());
-            clickedCard.setImageResource(id);
-
-            //add open card to array openedCards
-            addOpenedCard(positionInPack);
-            for (int i = 0; i < numOfMatchedCards; i++) {
-                Log.i("info", "openedCardsValues[" + i + "] = " + openedCardsValues[i]);
-            }
-
-
-            if (neededNumberOfCardsIsOpened()) {
-                if (areOpenedCardMatch()) {
-                    Log.i("info", "Cards are matched!!! ");
-
-                    removeMatchedCards();
-                    addPlayedCards();
-
-                    initOpenedCardsValuesArray();
-                    initOpenedCardsPositionsArray();
-
-                    MediaPlayer matchMediaPlayer = MediaPlayer.create(this, R.raw.memory_match);
-                    matchMediaPlayer.start();
-
-                    if (areAllCardsPlayed()) {
-                        Log.i("info", "WIN-WIN-WIN !!!");
-                        removeOnClickListenerOnImageViews();
-                        Toast toast = Toast.makeText(getApplicationContext(), "CONGRATULATIONS!!!", Toast.LENGTH_LONG);
-                        toast.show();
-                    }
-
-                } else {
-                    Handler handler = new Handler();
-                    handler.postDelayed(new Runnable() {
-                        public void run() {
-                            initOpenedCardsValuesArray();
-                            initOpenedCardsPositionsArray();
-                            turnAllCardsFaceDown();
-                        }
-                    }, 2000);
-
-                    flipMediaPlayer.start();
-
-                }
-            }
-
-            cntMoves++;
-            //showGameTime();
-            showMoves();
-        }
-        isAnimationInProgress = false;
-
-        Log.i("info", "...");
-    }
-
     private void removeOnClickListenerOnImageViews() {
         //Log.i("info", "setOnClickListenerOnImageViews...............");
         //remove onClickListener from each imageView
@@ -227,30 +122,87 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
     }
 
-    private boolean areAllCardsPlayed() {
-        boolean res = true;
-        int i=0;
-        while (i < lengthOfPack && res) {
-            if (!playedCards[i]) res = false;
-            i++;
+    public void onClick(View view) {
+        Log.i("info", "onClick...............");
+
+        if (!isPlayStarted){
+            setTimer();
+            isPlayStarted = true;
         }
-        return res;
+        ImageView clickedCard = (ImageView) view;
+
+        //Position of the clicked card in a whole pack
+        int positionInPack = Integer.parseInt(clickedCard.getTag().toString());
+        Log.i("info", "Position in Pack = " + positionInPack);
+
+        //Make sound
+        flipMediaPlayer = MediaPlayer.create(this, R.raw.memory_flip);
+        flipMediaPlayer.start();
+
+        //Turn the card face up (name of the target resource looks like "image<X>")
+        int id = getResources().getIdentifier("image" + pack[positionInPack - 1], "drawable", getPackageName());
+        clickedCard.setImageResource(id);
+        Log.i("info", "image" + pack[positionInPack - 1]);
+
+        //add position of the opened card to the array openedCards
+        cardTools.addOpenedCard(pack, positionInPack, openedCardsValues, openedCardsPositions, numOfMatchedCards);
+
+        if (cardTools.neededNumberOfCardsIsOpened(openedCardsValues, numOfMatchedCards)) {
+            if (cardTools.areOpenedCardMatch(openedCardsValues, numOfMatchedCards)) {
+                //Log.i("info", "neededNumberOfCardsIsOpened...Cards are matched!!! ");
+                removeMatchedCards();
+                cardTools.addPlayedCards(openedCardsPositions, playedCards, numOfMatchedCards);
+
+                openedCardsValues = cardTools.initOpenedCardsValuesArray(numOfMatchedCards);
+                openedCardsPositions = cardTools.initOpenedCardsPositionsArray(numOfMatchedCards);
+
+                MediaPlayer matchMediaPlayer = MediaPlayer.create(this, R.raw.memory_match);
+                matchMediaPlayer.start();
+
+                if (cardTools.areAllCardsPlayed(playedCards, lengthOfPack)) {
+                    Log.i("info", "WIN-WIN-WIN !!!");
+                    isPlayStarted = false;
+                    removeOnClickListenerOnImageViews();
+                    Toast toast = Toast.makeText(getApplicationContext(), "CONGRATULATIONS!!!", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+            else {
+                Handler handler = new Handler();
+                handler.postDelayed(new Runnable() {
+                    public void run() {
+                        openedCardsValues = cardTools.initOpenedCardsValuesArray(numOfMatchedCards);
+                        openedCardsPositions = cardTools.initOpenedCardsPositionsArray(numOfMatchedCards);
+                        turnAllCardsFaceDown();
+                    }
+                }, 1500);
+                flipMediaPlayer.start();
+            }
+        }
+
+        cntMoves++;
+        showMoves();
+        Log.i("info", "............................................................");
     }
 
-    private void addPlayedCards() {
-        for (int i = 0; i < numOfMatchedCards; i++) {
-            int position = openedCardsPositions[i];
-            playedCards[position-1] = true;
-        }
+    //Re-start the game
+    public void onStartClick(View view) {
+        Log.i("info", "onStartClick...............");
+        cardTools.shuffleCards(pack);
+        playedCards = cardTools.initPlayedCardsArray(lengthOfPack);
+        moveBackAllCards();
+        turnAllCardsFaceDown();
+        openedCardsValues = cardTools.initOpenedCardsValuesArray(numOfMatchedCards);
+        initTextMoves();
+        setOnClickListenerOnImageViews();
+        textTime.setText("Time: 00:00");
     }
-
-
 
     private void removeMatchedCards() {
         ImageView imageView;
         for (int i = 0; i < numOfMatchedCards; i++) {
             Log.i("info", "........removeMatchedCards...openedCardsPositions["+i+"] = " + openedCardsPositions[i]);
-            int resID = getResources().getIdentifier(getCardIDByPosition(openedCardsPositions[i]), "id", getPackageName());
+            int resID = getResources().getIdentifier(cardTools.getCardIDByPosition(openedCardsPositions[i], numCols), "id", getPackageName());
             imageView = findViewById(resID);
             imageView.animate().translationXBy(-1000f).setDuration(1000);
         }
@@ -267,106 +219,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
     }
 
-    private String getCardIDByPosition(int openedCardsPosition) {
-        Log.i("info", "........getCardIDByPosition...openedCardsPosition = " + openedCardsPosition);
-        int cardCol = openedCardsPosition % numCols;
-        int cardRow = openedCardsPosition / numCols;
-        if (cardCol == 0) {
-            cardCol = numCols;
-        }
-        else {
-            cardRow++;
-        }
-        String cardId = "card" + cardRow + cardCol;
-        Log.i("info", "........getCardIDByPosition...cardId = " + cardId);
-        return cardId;
-    }
-
-
-
-    /*private void showGameTime() {
-        TextView textTime = (TextView) findViewById(R.id.textTime);
-        textTime.setText("Time: " + timeInMilliseconds/1000 + " seconds");
-    }*/
-
     private void showMoves() {
         TextView textMoves = findViewById(R.id.textMoves);
         String strMoves = "Moves: " + cntMoves;
         textMoves.setText(strMoves);
     }
 
-    private boolean neededNumberOfCardsIsOpened() {   //3 cards <> 0
-        //Log.i("info", "neededNumberOfCardsIsOpened...............");
-        boolean res = true;
-        int i=0;
-        while (i<numOfMatchedCards && res) {
-            if (openedCardsValues[i] == 0) res = false;
-            i++;
-        }
-        return res;
-    }
-
-
-    private boolean areOpenedCardMatch() {
-        //Log.i("info", "areOpenedCardMatch...............");
-        boolean res = true;
-
-        int i = 1;
-        while (i < numOfMatchedCards && res) {
-            if (openedCardsValues[i] != openedCardsValues[i-1]) res = false;
-            Log.i("info", "openedCardsValues[" + i + "] = "+ openedCardsValues[i] + "    i = " + i + "   res = " + res);
-            i++;
-        }
-        return res;
-    }
-
-
-    private void addOpenedCard(int positionInPack) {
-        Log.i("info", "addOpenedCard...............pospositionInPackInPack = " + positionInPack);
-        int i = 0;
-        while (i<numOfMatchedCards && openedCardsValues[i] != 0) i++;
-        openedCardsValues[i] = pack[positionInPack-1];
-        openedCardsPositions[i] = positionInPack;
-    }
-
-
-    //Re-start the game
-    public void onStartClick(View view) {
-        //Log.i("info", "onStartClick...............");
-        shuffleCards();
-        initPlayedCardsArray();
-        moveBackAllCards();
-        turnAllCardsFaceDown();
-        initOpenedCardsValuesArray();
-        initGameTime();
-        initMoves();
-        setOnClickListenerOnImageViews();
-
-    }
-
-    private void initGameTime() {
-        timeInMilliseconds = 0;
-        TextView textTime = findViewById(R.id.textTime);
-        String str = "Time: 0 seconds";
-        textTime.setText(str);
-    }
-
-    private void initMoves() {
-        cntMoves = 0;
-        TextView textMoves = findViewById(R.id.textMoves);
-        String str = "Moves: 0";
-        textMoves.setText(str);
-    }
-
     //Turn all cards face down
-    public void turnAllCardsFaceDown() {
+    private void turnAllCardsFaceDown() {
         ///Log.i("info", "turnAllCardsFaceDown...............");
         for (int i = 1; i <= numRows; i++)
             for (int j =1; j <= numCols; j++)  turnCardFaceDown(i, j);
     }
 
     private void turnCardFaceDown(int i, int j) {
-
         ///Log.i("info", "turnCardFaceDown...............");
         int resID = getResources().getIdentifier("card" + i + j, "id", getPackageName());
         ImageView imageView = findViewById(resID);
@@ -374,4 +240,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = getResources().getIdentifier("square", "drawable", getPackageName());
         imageView.setImageResource(id);
     }
+
+    private void setTimer() {
+        final Handler h = new Handler();
+        h.postDelayed(new Runnable()
+        {
+            private long time = 0;
+            @Override
+            public void run()
+            {
+                time += 1000;
+                if (isPlayStarted) {
+                    textTime.setText("Time: " + cardTools.getTextTime(time));
+                }
+                h.postDelayed(this, 1000);
+            }
+        }, 1000); // 1 second
+    }
+
 }
---
+++ app/src/main/java/com/packtpub/testlayouts/cardTools.java
@@ -0,0 +1,137 @@
+package com.packtpub.testlayouts;
+
+import android.os.Handler;
+import android.util.Log;
+
+import java.util.Random;
+
+public class cardTools {
+
+    public static int[] initPackArray(int lengthOfPack, int numberOfCardsInSet){
+        // Log.i("info", "initPackArray...............");
+        int[] pack = new int[lengthOfPack];
+        for (int i = 0; i < lengthOfPack; i++) {    //Initialising pack array. For level #1 = [1;2;3;4;5;6;1;2;3;4;5;6]
+            pack[i] = (i + 1) % numberOfCardsInSet + 1;
+            Log.i("info", "initPackArray...............pack["+i+"]="+pack[i]);
+        }
+        return pack;
+    }
+
+    public static boolean[] initPlayedCardsArray(int lengthOfPack) {
+        // Log.i("info", "initPlayedCardsArray...............");
+        boolean[] playedCards = new boolean[lengthOfPack];
+        for (int i = 0; i < lengthOfPack; i++) {
+            playedCards[i] = false;
+        }
+        return playedCards;
+    }
+
+    public static int[] initOpenedCardsValuesArray(int numOfMatchedCards){
+        /// Log.i("info", "initOpenedCardsValuesArray...............");
+        int[] openedCardsValues = new int[numOfMatchedCards];
+        for (int i = 0; i < numOfMatchedCards; i++) {
+            openedCardsValues[i] = 0;
+        }
+        return openedCardsValues;
+    }
+
+    public static int[] initOpenedCardsPositionsArray(int numOfMatchedCards) {
+        /// Log.i("info", "initOpenedCardsPositionsArray...............");
+        int[] openedCardsPositions = new int[numOfMatchedCards];
+        for (int i = 0; i < numOfMatchedCards; i++) {
+            openedCardsPositions[i] = 0;
+        }
+        return openedCardsPositions;
+    }
+
+    public static int[] shuffleCards(int[] pack) {
+        // Log.i("info", "shuffleCards...............");
+        Random random = new Random();
+
+        for (int i = 0; i < pack.length; i++) {
+            int cardPosition = random.nextInt(pack.length - 1);
+            int curCard = pack[i];
+            pack[i] = pack[cardPosition];
+            pack[cardPosition] = curCard;
+        }
+        return pack;
+    }
+
+    public static boolean areAllCardsPlayed(boolean[] playedCards, int lengthOfPack) {
+        boolean res = true;
+        int i=0;
+        while (i < lengthOfPack && res) {
+            if (!playedCards[i]) res = false;
+            i++;
+        }
+        return res;
+    }
+
+    public static boolean[] addPlayedCards(int[] openedCardsPositions, boolean[] playedCards, int numOfMatchedCards) {
+        for (int i = 0; i < numOfMatchedCards; i++) {
+            int position = openedCardsPositions[i];
+            playedCards[position-1] = true;
+        }
+        return playedCards;
+    }
+
+    public static String getCardIDByPosition(int openedCardsPosition, int numCols) {
+        //Log.i("info", "........getCardIDByPosition...openedCardsPosition = " + openedCardsPosition);
+        int cardCol = openedCardsPosition % numCols;
+        int cardRow = openedCardsPosition / numCols;
+        if (cardCol == 0) {
+            cardCol = numCols;
+        }
+        else {
+            cardRow++;
+        }
+        String cardId = "card" + cardRow + cardCol;
+        //Log.i("info", "........getCardIDByPosition...cardId = " + cardId);
+        return cardId;
+    }
+
+    public static boolean neededNumberOfCardsIsOpened(int[] openedCardsValues, int numOfMatchedCards) {
+        //Log.i("info", "neededNumberOfCardsIsOpened...............");
+        boolean res = true;
+        int i = 0;
+        while (i<numOfMatchedCards && res) {
+            if (openedCardsValues[i] == 0) res = false;
+            i++;
+        }
+        return res;
+    }
+
+    public static boolean areOpenedCardMatch(int[] openedCardsValues, int numOfMatchedCards) {
+        //Log.i("info", "areOpenedCardMatch...............");
+        boolean res = true;
+        int i = 1;
+        while (i < numOfMatchedCards && res) {
+            if (openedCardsValues[i] != openedCardsValues[i-1]) res = false;
+            //Log.i("info", "openedCardsValues[" + i + "] = "+ openedCardsValues[i] + "    i = " + i + "   match previous = " + res);
+            i++;
+        }
+        return res;
+    }
+
+    public static void addOpenedCard(int[] pack, int positionInPack, int[] openedCardsValues, int[] openedCardsPositions, int numOfMatchedCards) {
+        Log.i("info", "addOpenedCard...............numOfMatchedCards = " + numOfMatchedCards);
+        int i = 0;
+        while (i < numOfMatchedCards   &&   openedCardsValues[i] != 0) {
+            Log.i("info", "addOpenedCard...............openedCardsValues["+i+"] = " + openedCardsValues[i]);
+            i++;
+        }
+        openedCardsValues[i] = pack[positionInPack-1];
+        openedCardsPositions[i] = positionInPack;
+    }
+
+    public static String getTextTime(long time){
+        int intTime = (int) time / 1000;
+        int minutes = (int) intTime / 60;
+        int seconds = (int) intTime % 60;
+        String addMinutes = "";
+        String addSeconds = "";
+        if (minutes < 10) addMinutes = "0";
+        if (seconds < 10) addSeconds = "0";
+        return addMinutes + minutes + ":" + addSeconds + seconds;
+    }
+}
